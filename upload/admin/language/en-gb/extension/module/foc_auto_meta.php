edb522443a2d6f46dab7f2524609300e3344f97c
ikenfin <ikenfin@gmail.com>
2018-07-03 22:00:33 +0400

    :speech_balloon: added i18n

---
+++ upload/admin/language/en-gb/extension/module/foc_auto_meta.php
@@ -0,0 +1,12 @@
+<?php
+
+$_['heading_title'] = 'FOC: Auto meta';
+
+$_['text_extension'] = 'Extensions';
+$_['text_module'] = 'Modules';
+
+$_['field_foc_auto_meta_product_title'] = 'Product title template';
+$_['field_foc_auto_meta_product_description'] = 'Product description template';
+$_['field_foc_auto_meta_category_title'] = 'Category title template';
+$_['field_foc_auto_meta_category_description'] = 'Category description template';
+$_['force_replace'] = 'Force replace value with rendered template';

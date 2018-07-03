3a03f59325a78baf9c44a307ee7df29a38428b07
ikenfin <ikenfin@gmail.com>
2018-07-03 19:46:57 +0400

    gulpfile fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
-const gulp = require('gulp'),
-      zip = require('gulp-zip');
+const gulp = require('gulp');
 
 gulp.task('mktest', () => {
   gulp.src('./upload/**')
       .pipe(gulp.dest('../../oc2/'));
 });
 
-gulp.task('test', ['mktest'], () => {
+gulp.task('default', ['mktest'], () => {
   gulp.watch('./upload/**', () => {
     gulp.start('mktest');
   });

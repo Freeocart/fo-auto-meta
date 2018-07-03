13ba59982f2b6c58b2258674501027a7f2799ca1
ikenfin <ikenfin@gmail.com>
2018-07-03 20:21:53 +0400

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 
 gulp.task('mktest', () => {
   gulp.src('./upload/**')
-      .pipe(gulp.dest('../../oc2/'));
+      .pipe(gulp.dest('../oc2/'));
 });
 
 gulp.task('default', ['mktest'], () => {

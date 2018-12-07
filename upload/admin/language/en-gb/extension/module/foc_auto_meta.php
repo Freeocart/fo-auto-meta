51ea180c579ccf3e8a2cbd284b7c496c03c1bfd7
ikenfin <ikenfin@gmail.com>
2018-12-08 00:01:16 +0400

    :speech_balloon: added english translations

--- upload/admin/language/en-gb/extension/module/foc_auto_meta.php
+++ upload/admin/language/en-gb/extension/module/foc_auto_meta.php
@@ -10,3 +10,25 @@ $_['field_foc_auto_meta_product_description'] = 'Product description template';
 $_['field_foc_auto_meta_category_title'] = 'Category title template';
 $_['field_foc_auto_meta_category_description'] = 'Category description template';
 $_['force_replace'] = 'Force replace value with rendered template';
+
+// additional
+$_['field_foc_auto_meta_contacts_page_title'] = 'Contacts page title';
+$_['field_foc_auto_meta_contacts_page_description'] = 'Contacts page description';
+
+$_['field_foc_auto_meta_reviews_title'] = 'Reviews page title';
+$_['field_foc_auto_meta_reviews_description'] = 'Reviews page description';
+
+$_['field_foc_auto_meta_sitemap_title'] = 'Sitemap page title';
+$_['field_foc_auto_meta_sitemap_description'] = 'Sitemap page description';
+
+$_['field_foc_auto_meta_products_special_title'] = 'Products special page title';
+$_['field_foc_auto_meta_products_special_description'] = 'Products special page description';
+
+$_['field_foc_auto_meta_voucher_title'] = 'Voucher page title';
+$_['field_foc_auto_meta_voucher_description'] = 'Voucher page description';
+
+$_['field_foc_auto_meta_faq_title'] = 'FAQ module page title';
+$_['field_foc_auto_meta_faq_description'] = 'FAQ module page description';
+
+$_['field_foc_auto_meta_manufacturer_title'] = 'Manufacturer page title';
+$_['field_foc_auto_meta_manufacturer_description'] = 'Manufacturer page description';

6402f365438d7fd6be7550826df9d0eeced476fb
ikenfin <ikenfin@gmail.com>
2019-05-28 20:19:25 +0400

    split build

--- pack.sh
+++ pack.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-out=foc_auto_meta.ocmod.zip
+out=foc_auto_meta30.ocmod.zip
+out2=foc_auto_meta23.ocmod.zip
 if [[ -f "$out" ]]; then
   rm $out
 fi
-zip -r9 --exclude=*.git* --exclude=*.DS_Store* $out ./upload ./install.xml
+if [[ -f "$out2" ]]; then
+  rm $out2
+fi
+
+zip -r9 --exclude=*.git* --exclude=*.DS_Store* --exclude=*.tpl $out ./upload ./install.xml
+zip -r9 --exclude=*.git* --exclude=*.DS_Store* --exclude=*.twig $out2 ./upload ./install.xml

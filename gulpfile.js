5c87f38abb03362f8595f7b5de887581c2d2d65f
ikenfin <ikenfin@gmail.com>
2020-04-16 19:49:34 +0400

    migrate to gulp4

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 const gulp = require('gulp');
 
-gulp.task('mktest', () => {
-  gulp.src('./upload/**')
-      .pipe(gulp.dest('../oc2/'));
-});
+function mktest () {
+  return gulp.src('./upload/**')
+    .pipe(gulp.dest('../oc2/'))
+}
 
-gulp.task('default', ['mktest'], () => {
-  gulp.watch('./upload/**', () => {
-    gulp.start('mktest');
-  });
-});
+function watchMkTest () {
+  return gulp.watch('./upload/**', mktest)
+}
+
+gulp.task('mktest', mktest);
+gulp.task('default', gulp.series(mktest, watchMkTest));
